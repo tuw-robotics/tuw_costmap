707c2b534e28471aeb68f526516d5eee310ac7cd
Markus Bader <markus.bader@tuwien.ac.at>
2024-08-22 13:49:30 +0200

    freespace first version working

--- nav2_costmap_freespace_plugin/include/nav2_costmap_freespace_plugin/freespace_layer.hpp
+++ nav2_costmap_freespace_plugin/include/nav2_costmap_freespace_plugin/freespace_layer.hpp
@@ -1,45 +1,3 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2008, 2013, Willow Garage, Inc.
- *  Copyright (c) 2020, Samsung R&D Institute Russia
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Eitan Marder-Eppstein
- *         David V. Lu!!
- *         Alexey Merzlyakov
- *
- * Reference tutorial:
- * https://navigation.ros.org/tutorials/docs/writing_new_costmap2d_plugin.html
- *********************************************************************/
 #ifndef FREESPACE_LAYER_HPP_
 #define FREESPACE_LAYER_HPP_
 
@@ -103,6 +61,8 @@ void updateWithFreeSpace( nav2_costmap_2d::Costmap2D & master_grid, int min_i, i
    */
   void incomingMap(const nav_msgs::msg::OccupancyGrid::SharedPtr new_map);
 
+  void createLookupTable();
+
   /**
    * @brief Callback to update the costmap's map from the map_server (or SLAM)
    * with an update in a particular area of the map
@@ -145,6 +105,9 @@ private:
   int GRADIENT_SIZE = 20;
   // Step of increasing cost per one cell in gradient
   int GRADIENT_FACTOR = 10;
+
+  // to update the master of with the current layer master x,y -> costmap idx 
+  std::vector<unsigned int> lookup_;  
 };
 
 }  // namespace nav2_costmap_freespace_plugin
--- nav2_costmap_freespace_plugin/src/freespace_layer.cpp
+++ nav2_costmap_freespace_plugin/src/freespace_layer.cpp
@@ -1,51 +1,11 @@
-/*********************************************************************
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2008, 2013, Willow Garage, Inc.
- *  Copyright (c) 2020, Samsung R&D Institute Russia
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Eitan Marder-Eppstein
- *         David V. Lu!!
- *         Alexey Merzlyakov
- *
- * Reference tutorial:
- * https://navigation.ros.org/tutorials/docs/writing_new_costmap2d_plugin.html
- *********************************************************************/
 #include "nav2_costmap_freespace_plugin/freespace_layer.hpp"
 
 #include "nav2_costmap_2d/costmap_math.hpp"
 #include "nav2_costmap_2d/footprint.hpp"
 #include "rclcpp/parameter_events_filter.hpp"
 #include "nav2_util/validate_messages.hpp"
+#include "tf2/convert.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 using nav2_costmap_2d::LETHAL_OBSTACLE;
 using nav2_costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
@@ -121,12 +81,6 @@ FreespaceLayer::updateBounds(
     map_buffer_ = nullptr;
   }
 
-  if (!layered_costmap_->isRolling() ) {
-    if (!(has_updated_data_ || has_extra_bounds_)) {
-      return;
-    }
-  }
-
   useExtraBounds(min_x, min_y, max_x, max_y);
 
   double wx, wy;
@@ -168,26 +122,17 @@ void FreespaceLayer::updateWithFreeSpace(
   unsigned char * master_array = master_grid.getCharMap();
   unsigned int span = master_grid.getSizeInCellsX();
 
-  std::map<int, int> hist;
   for (int j = min_j; j < max_j; j++) {
     unsigned int it = j * span + min_i;
     for (int i = min_i; i < max_i; i++) {
-      unsigned char new_cost =  costmap_[it];
-      unsigned char old_cost = master_array[it];
-      auto entry = hist.find(new_cost);
-      if (entry != hist.end())  hist[new_cost] = hist[new_cost] + 1;
-      else  hist[new_cost] = 1;
-      
-      if (new_cost == nav2_costmap_2d::FREE_SPACE) {
-        master_array[it] = nav2_costmap_2d::FREE_SPACE;
+      unsigned char value =  costmap_[it];
+      unsigned char *cost = &master_array[it];      
+      if (value == nav2_costmap_2d::FREE_SPACE) {
+        *cost = nav2_costmap_2d::FREE_SPACE;
       }
       it++;
     }
   } 
-  std::cout << "---" << std::endl;
-  for (const auto& pair : hist) {
-        std::cout << "Key: " << (int) pair.first << ", Value: " << pair.second << std::endl;
-  }
 }
 
 // The method is called when costmap recalculation is required.
@@ -209,18 +154,16 @@ FreespaceLayer::updateCosts(
     static int count = 0;
     // throttle warning down to only 1/10 message rate
     if (++count == 10) {
-      RCLCPP_WARN(logger_, "FreespaceLayer: Can't update static costmap layer, no map received");
+      RCLCPP_DEBUG(logger_, "FreespaceLayer: Can't update static costmap layer, no map received");
       count = 0;
     }
     return;
   }
 
-  if (!layered_costmap_->isRolling()) {
-    RCLCPP_INFO(logger_, "FreespaceLayer: !isRolling");
-    // if not rolling, the layered costmap (master_grid) has same coordinates as this layer
-    updateWithFreeSpace(master_grid, min_i, min_j, max_i, max_j);
-  } else {
-    RCLCPP_INFO(logger_, "FreespaceLayer: isRolling");
+  {
+    /// @ToDo optimierung mit lookuptable 
+
+    RCLCPP_DEBUG(logger_, "FreespaceLayer: isRolling");
     // If rolling window, the master_grid is unlikely to have same coordinates as this layer
     unsigned int mx, my;
     double wx, wy;
@@ -236,6 +179,7 @@ FreespaceLayer::updateCosts(
     }
     // Copy map data given proper transformations
     tf2::Transform tf2_transform;
+
     tf2::fromMsg(transform.transform, tf2_transform);
 
     for (int i = min_i; i < max_i; ++i) {
@@ -258,6 +202,51 @@ FreespaceLayer::updateCosts(
   current_ = true;
 }
 
+void
+FreespaceLayer::createLookupTable(){
+  
+    RCLCPP_INFO(logger_, "FreespaceLayer: createLookupTable");
+  Costmap2D * master = layered_costmap_->getCostmap();
+  unsigned int size_x = master->getSizeInCellsX();
+  unsigned int size_y = master->getSizeInCellsY();
+  lookup_.resize(size_x*size_y);
+
+    geometry_msgs::msg::TransformStamped transform;
+    try {
+      transform = tf_->lookupTransform(
+        map_frame_, global_frame_, tf2::TimePointZero,
+        transform_tolerance_);
+    } catch (tf2::TransformException & ex) {
+      RCLCPP_ERROR(logger_, "FreespaceLayer: %s", ex.what());
+      return;
+    }
+    // Copy map data given proper transformations
+    tf2::Transform tf2_transform;
+
+    tf2::fromMsg(transform.transform, tf2_transform);
+
+    // If rolling window, the master_grid is unlikely to have same coordinates as this layer
+    unsigned int mx, my;
+    double wx, wy;
+
+    unsigned int idx = 0;
+    for (unsigned int i = 0; i < size_y; ++i) {
+      for (unsigned int j = 0; j < size_x; ++j) {
+        // Convert master_grid coordinates (i,j) into global_frame_(wx,wy) coordinates
+        layered_costmap_->getCostmap()->mapToWorld(i, j, wx, wy);
+        // Transform from global_frame_ to map_frame_
+        tf2::Vector3 p(wx, wy, 0);
+        p = tf2_transform * p;
+        // Set master_grid with cell from map
+        unsigned int value = std::numeric_limits<unsigned int>::max();
+        if (worldToMap(p.x(), p.y(), mx, my)) {
+          value = getIndex(mx, my);
+        } 
+        lookup_[idx++] = value;
+      }
+    }
+}
+
 unsigned char
 FreespaceLayer::interpretValue(unsigned char value)
 {
@@ -271,36 +260,17 @@ FreespaceLayer::interpretValue(unsigned char value)
 void
 FreespaceLayer::processMap(const nav_msgs::msg::OccupancyGrid & new_map)
 {
-  RCLCPP_DEBUG(logger_, "FreespaceLayer: Process map");
 
   unsigned int size_x = new_map.info.width;
   unsigned int size_y = new_map.info.height;
 
-  RCLCPP_DEBUG(
+  RCLCPP_INFO(
     logger_,
-    "FreespaceLayer: Received a %d X %d map at %f m/pix", size_x, size_y,
+    "FreespaceLayer: processMap a %d X %d map at %f m/pix", size_x, size_y,
     new_map.info.resolution);
 
   // resize costmap if size, resolution or origin do not match
-  Costmap2D * master = layered_costmap_->getCostmap();
-  if (!layered_costmap_->isRolling() && (master->getSizeInCellsX() != size_x ||
-    master->getSizeInCellsY() != size_y ||
-    master->getResolution() != new_map.info.resolution ||
-    master->getOriginX() != new_map.info.origin.position.x ||
-    master->getOriginY() != new_map.info.origin.position.y ||
-    !layered_costmap_->isSizeLocked()))
-  {
-    // Update the size of the layered costmap (and all layers, including this one)
-    RCLCPP_INFO(
-      logger_,
-      "FreespaceLayer: Resizing costmap to %d X %d at %f m/pix", size_x, size_y,
-      new_map.info.resolution);
-    layered_costmap_->resizeMap(
-      size_x, size_y, new_map.info.resolution,
-      new_map.info.origin.position.x,
-      new_map.info.origin.position.y,
-      true);
-  } else if (size_x_ != size_x || size_y_ != size_y ||  // NOLINT
+  if (size_x_ != size_x || size_y_ != size_y ||  // NOLINT
     resolution_ != new_map.info.resolution ||
     origin_x_ != new_map.info.origin.position.x ||
     origin_y_ != new_map.info.origin.position.y)
@@ -321,23 +291,13 @@ FreespaceLayer::processMap(const nav_msgs::msg::OccupancyGrid & new_map)
 
   RCLCPP_INFO(logger_, "FreespaceLayer: initialize the costmap with static data");
   // initialize the costmap with static data
-  std::map<unsigned char, int> hist;
   for (unsigned int i = 0; i < size_y; ++i) {
     for (unsigned int j = 0; j < size_x; ++j) {
       unsigned char value = new_map.data[index];
-      auto entry = hist.find(value);
-      if (entry != hist.end())  hist[value] = hist[value] + 1;
-      else  hist[value] = 1;
-
       costmap_[index] = interpretValue(value);
       ++index;
     }
   }
-  std::cout << "--- interpretValues -- " << std::endl;
-  for (const auto& pair : hist) {
-        std::cout << "Key: " << (int) pair.first << ", Value: " << pair.second << std::endl;
-  }
-
   map_frame_ = new_map.header.frame_id;
 
   x_ = y_ = 0;
